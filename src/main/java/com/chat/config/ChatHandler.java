9a17ed8b55797c928a487e93bdbbf9cb89d4ceb2
anandofficial42 <anandofficial42@gmail.com>
2026-02-24 06:15:42 +0530

    chat server

---
+++ src/main/java/com/chat/config/ChatHandler.java
@@ -0,0 +1,30 @@
+package com.chat.config;
+
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.*;
+
+public class ChatHandler extends TextWebSocketHandler {
+
+    private static final Set<WebSocketSession> sessions = new HashSet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+}
